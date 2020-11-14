e34d5033fd4a7b181994f6b06331ac54dc48c6ce
Alin Ciu <alin.ciu@endava.com>
2020-11-14 13:11:51 +0200

    -

--- kubectl_delete_all.bat
+++ kubectl_delete_all.bat
@@ -1,7 +1,9 @@
 @echo off
+SET KUBECONFIG=%CD%\\kubeconfig.yaml
 cd ../
-kubectl delete -f grpc-poc-common/kubernetes.yml --kubeconfig=kubeconfig.yml
-kubectl delete -f grpc-poc-ui-preact/kubernetes.yml --kubeconfig=kubeconfig.yml
-kubectl delete -f grpc-poc-weather-java/kubernetes.yml --kubeconfig=kubeconfig.yml
-kubectl delete -f grpc-poc-darksky-mock-node/kubernetes.yml --kubeconfig=kubeconfig.yml
-kubectl delete -f grpc-poc-auth-dotnet/kubernetes.yml --kubeconfig=kubeconfig.yml
+
+kubectl delete -f grpc-poc-common/kubernetes.yaml
+kubectl delete -f grpc-poc-ui-preact/kubernetes.yaml
+kubectl delete -f grpc-poc-weather-java/kubernetes.yaml
+kubectl delete -f grpc-poc-darksky-mock-node/kubernetes.yaml
+kubectl delete -f grpc-poc-auth-dotnet/kubernetes.yaml
--- kubectl_deploy_all.bat
+++ kubectl_deploy_all.bat
@@ -1,7 +1,9 @@
 @echo off
+SET KUBECONFIG=%CD%\\kubeconfig.yaml
 cd ../
-kubectl apply -f grpc-poc-common/kubernetes.yml --kubeconfig=kubeconfig.yml
-kubectl apply -f grpc-poc-ui-preact/kubernetes.yml --kubeconfig=kubeconfig.yml
-kubectl apply -f grpc-poc-weather-java/kubernetes.yml --kubeconfig=kubeconfig.yml
-kubectl apply -f grpc-poc-darksky-mock-node/kubernetes.yml --kubeconfig=kubeconfig.yml
-kubectl apply -f grpc-poc-auth-dotnet/kubernetes.yml --kubeconfig=kubeconfig.yml
+
+kubectl apply -f grpc-poc-common/kubernetes.yaml
+kubectl apply -f grpc-poc-ui-preact/kubernetes.yaml
+kubectl apply -f grpc-poc-weather-java/kubernetes.yaml
+kubectl apply -f grpc-poc-darksky-mock-node/kubernetes.yaml
+kubectl apply -f grpc-poc-auth-dotnet/kubernetes.yaml
