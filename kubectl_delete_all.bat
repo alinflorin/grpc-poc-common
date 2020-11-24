2d42c34a7a05709d2be331f054e574099ab55051
Alin Ciu <alin.ciu@endava.com>
2020-11-25 01:37:03 +0200

    -

--- kubectl_delete_all.bat
+++ kubectl_delete_all.bat
@@ -2,8 +2,8 @@
 
 cd ../
 
-kubectl delete -f grpc-poc-common/kubernetes.yaml -n grpc-poc
-kubectl delete -f grpc-poc-ui-preact/kubernetes.yaml -n grpc-poc
-kubectl delete -f grpc-poc-weather-java/kubernetes.yaml -n grpc-poc
-kubectl delete -f grpc-poc-darksky-mock-node/kubernetes.yaml -n grpc-poc
-kubectl delete -f grpc-poc-auth-dotnet/kubernetes.yaml -n grpc-poc
+kubectl delete -f grpc-poc-common/kubernetes.yaml
+kubectl delete -f grpc-poc-ui-preact/kubernetes.yaml
+kubectl delete -f grpc-poc-weather-java/kubernetes.yaml
+kubectl delete -f grpc-poc-darksky-mock-node/kubernetes.yaml
+kubectl delete -f grpc-poc-auth-dotnet/kubernetes.yaml
--- kubectl_deploy_all.bat
+++ kubectl_deploy_all.bat
@@ -2,8 +2,8 @@
 
 cd ../
 
-kubectl apply -f grpc-poc-common/kubernetes.yaml -n grpc-poc
-kubectl apply -f grpc-poc-ui-preact/kubernetes.yaml -n grpc-poc
-kubectl apply -f grpc-poc-weather-java/kubernetes.yaml -n grpc-poc
-kubectl apply -f grpc-poc-darksky-mock-node/kubernetes.yaml -n grpc-poc
-kubectl apply -f grpc-poc-auth-dotnet/kubernetes.yaml -n grpc-poc
+kubectl apply -f grpc-poc-common/kubernetes.yaml
+kubectl apply -f grpc-poc-ui-preact/kubernetes.yaml
+kubectl apply -f grpc-poc-weather-java/kubernetes.yaml
+kubectl apply -f grpc-poc-darksky-mock-node/kubernetes.yaml
+kubectl apply -f grpc-poc-auth-dotnet/kubernetes.yaml
